288cc60eb96352c05354df985eb120de607d33df
Obed-Makori <makoriobed95@gmail.com>
2024-07-16 15:59:41 +0300

    Mid July version

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
+# import pandas as pd
+# import pandas_profiling
+# import streamlit as st
+# from streamlit_pandas_profiling import st_profile_report
+
+# st.set_page_config(layout='wide')
+
+# df = pd.read_csv('Salary Prediction dataset.csv')
+# pr = df.profile_report()
+
+# st_profile_report(pr)
+
 import streamlit as st
-import pandas as pd
-import seaborn as sns
-df3=pd.read_csv('df.csv')
 
-import matplotlib.pyplot as plt
-
-fig, ax = plt.subplots()
-sns.heatmap(df3.corr(), ax=ax)
-st.write(fig)
+st.logo(LOGO_URL_LARGE, link="https://streamlit.io/gallery", icon_image=LOGO_URL_SMALL)
