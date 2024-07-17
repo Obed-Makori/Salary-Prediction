b43037393253425668db1101153cdf9676177a9c
Obed-Makori <makoriobed95@gmail.com>
2024-07-17 16:35:01 +0300

    evening version

--- test.py
+++ test.py
@@ -1,15 +1,12 @@
-# import pandas as pd
-# import pandas_profiling
-# import streamlit as st
-# from streamlit_pandas_profiling import st_profile_report
-
-# st.set_page_config(layout='wide')
-
-# df = pd.read_csv('Salary Prediction dataset.csv')
-# pr = df.profile_report()
-
-# st_profile_report(pr)
-
 import streamlit as st
+with st.sidebar:
+  st.image("mkenzo-logo.png", width=250)
+  st.markdown("<style> margin-top:0; </style>",unsafe_allow_html=True)
+  st.multiselect('Select Age', [1,2,3,4,5,6,7,8,9,'Skip'])
+  st.button('Button 1')
+import random
+import numpy as np
+s=np.random.rand()
+st.button('Test')
+# st.markdown("<h4 style=color:red; text-color:red;>TOP PLAYER</h2>", unsafe_allow_html=True) 
 
-st.logo(LOGO_URL_LARGE, link="https://streamlit.io/gallery", icon_image=LOGO_URL_SMALL)
---
+++ utils.py
@@ -0,0 +1,4 @@
+from streamlit_extras.app_logo import add_logo
+
+def logo():
+    add_logo("mkenzo-logo.png")
