070cd1fa1970ef6cb620ff12ad22742adac56596
Obed-Makori <makoriobed95@gmail.com>
2024-07-18 23:34:18 +0300

    version n

--- utils.py
+++ utils.py
@@ -1,4 +1,9 @@
-from streamlit_extras.app_logo import add_logo
+# from streamlit_extras.app_logo import add_logo
 
-def logo():
-    add_logo("mkenzo-logo.png")
+# def logo():
+#     add_logo("mkenzo-logo.png")
+
+import streamlit as st
+
+
+st.markdown("<h2 style='color: red'>TEST</h2>", unsafe_allow_html=True)
